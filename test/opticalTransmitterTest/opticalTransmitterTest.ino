4cc7575163207d7cd366f432f764ed03e355c654
chrisdats <christopher.datsikas@gmail.com>
2016-03-20 20:20:39 -0400

    working optical test code -- led and opticalTx blink simultaneously, serial monitor communication works

--- test/opticalTransmitterTest/opticalTransmitterTest.ino
+++ test/opticalTransmitterTest/opticalTransmitterTest.ino
@@ -1,62 +1,96 @@
-/* LED Blink, Teensyduino Tutorial #1
-   http://www.pjrc.com/teensy/tutorial.html
+/* opticalTransmitterTest.ino 
+ Christopher Datsikas
+ Adapted from Teensy documentation, BlinkWithoutDelay.ino Arduino Example, Geek Group
+ Date Created: 03-20-2016
+ Date Modified: 03-20-2016
+ Status: Works!
+ Run this code to verify operation of optical transmitter
+ IF-E96E should blink red
+ AND On-Board LED will blink to show that Board has power and is working
  
-   This example code is in the public domain.
-*/
+ EQUIPMENT
+ Teensy 3.2
+ IF-E96E 
+ 330 Ohm Resistor
+ ULN2803A Darlington Transistor Array
+ 
+ CONNECTIONS
+ Teensy D3           -> ULN2803A Pin 1B/1
+ ULN2803A Pin 1C/18  -> 330 Ohm Resistor -> IF-E96E Cathode (-) (short)
+ IF-E96E Anode (+) (long) <-> VIN_3V3
+ */
+ 
+#define ledPin   13             // On-board Chip LED
+#define outPin   3              // Sinks IF-E96E
 
-// Teensy 2.0 has the LED on pin 11
-// Teensy++ 2.0 has the LED on pin 6
-// Teensy 3.0 has the LED on pin 13
-#define OutPin   3
-#define ledPin   13
-float freq = 20;          //Change the frequency of the tone here
-float period;
-float duty = .50;          //Change the duty cycle of the tone here
-unsigned long ontime, offtime, start;
-
-// Variables will change:
+// LED variables
 int ledState = LOW;             // ledState used to set the LED
 long previousMillis = 0;        // will store last time LED was updated
-long interval = 1000;           // interval at which to blink (milliseconds)
+long ledInterval = 1000;           // interval at which to blink (milliseconds)
+
+// Tx variables
+float freq = 10;                // Change the frequency of the tone here (in Hz)
+float period;
+float duty = .50;               //Change the duty cycle of the tone here
+int txState = LOW;
+unsigned long ontime, offtime;
+unsigned long currentMicros = 0;    // will overflow after 70 minutes
+unsigned long previousMicros = 0;
+unsigned long txInterval = offtime;
 
 void setup() {
-  // set the digital pin as output:
+  // set the digital pin as output;
   pinMode(ledPin, OUTPUT);
-  pinMode(OutPin, OUTPUT);
-  period=1/freq;
-  ontime=1000000*period*duty;         // in us
-  offtime=1000000*period*(1.0-duty);  // in us
+  pinMode(outPin, OUTPUT);
+  period = 1/freq;                      // period is inverse of frequency (math)
+  ontime = 1000000*period*duty;         // in us
+  offtime = 1000000*period*(1.0-duty);  // in us
+  
+  delay(100);
+  // for debugging
+  Serial.begin(9600);
+  while (!Serial);         // while the serial stream is not open, do nothing
+  Serial.println(period);
+  Serial.println(ontime);
+  Serial.println(offtime);
 }
 
 void loop()
 {
-  // here is where you'd put code that needs to be running all the time.
-
+  
   // check to see if it's time to blink the LED; that is, if the 
   // difference between the current time and last time you blinked 
-  // the LED is bigger than the interval at which you want to 
-  // blink the LED.
+  // the LED is bigger than the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > ledInterval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
   
-//  unsigned long currentMillis = millis();
-//  if(currentMillis - previousMillis > interval) {
-//    // save the last time you blinked the LED 
-//    previousMillis = currentMillis;   
-//
-//    // if the LED is off turn it on and vice-versa:
-//    if (ledState == LOW)
-//      ledState = HIGH;
-//    else
-//      ledState = LOW;
-//
-//    // set the LED with the ledState of the variable:
-//    digitalWrite(ledPin, ledState);
-//  }
   
-  start=micros();
-  digitalWrite(OutPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  while (micros()<start+ontime);
-  digitalWrite(OutPin, LOW);    // turn the LED off by making the voltage LOW
-  while (micros()<start+ontime+offtime);
-  
+  currentMicros = micros();
+  if (currentMicros - previousMicros > txInterval) {
+    previousMicros = currentMicros;
+    
+    if (txState == LOW) {
+      txState = HIGH;
+      txInterval = ontime;
+    }
+    else if (txState == HIGH) {
+      txState = LOW;
+      txInterval = offtime;
+    }
+    
+    digitalWrite(outPin, txState);
+  }
   
 }
